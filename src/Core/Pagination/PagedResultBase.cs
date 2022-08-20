783f6d789cb5ab1c449738b7093c9992836f75e5
GPereira <gugupereira123@hotmail.com>
2022-08-20 19:54:02 -0300

    feat(#3): Created base pagination

---
+++ src/Core/Pagination/PagedResultBase.cs
@@ -0,0 +1,9 @@
+namespace Core.Pagination;
+
+public abstract class PagedResultBase
+{
+    public int CurrentPage { get; set; }
+    public int PageCount { get; set; }
+    public int PageSize { get; set; }
+    public int RowCount { get; set; }
+}
